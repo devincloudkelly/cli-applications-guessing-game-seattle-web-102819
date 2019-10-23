22a0d37f013919f4d71127070e03fd4d80619e6b
Devin Kelly <devin.cloud.kelly@gmail.com>
2019-10-23 18:27:55 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,8 +1,11 @@
 # Code your solution here!
+require 'pry'
+
 def run_guessing_game
   comp_num = rand(1..6)
   puts "Guess a number between 1 and 6."
   user_num = gets.chomp!
+  binding.pry
   if comp_num == user_num
     puts "You guessed the correct number!"
   end
